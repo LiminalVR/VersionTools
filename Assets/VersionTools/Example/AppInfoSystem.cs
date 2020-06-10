d44f3f35f263a35d53579929ad947a7998fa6078
tinny <tinnystudios@gmail.com>
2020-06-10 10:48:53 +1000

    Initial Commit

---
+++ Assets/VersionTools/Example/AppInfoSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BuildTools;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AppInfoSystem : MonoBehaviour
+{
+    public VersionSettings VersionSettings;
+
+    public Text VersionLabel;
+
+    private void Start()
+    {
+        VersionLabel.text = VersionSettings.Version.ToString();
+    }
+}
